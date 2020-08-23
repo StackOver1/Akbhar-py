4fd85a07ba6c934fbd502d8396a2b0fe7bbef739
StackOver1 <farhanjamadar101@gmail.com>
2020-08-23 11:40:43 +0530

    Akbhar.py project

---
+++ akbhar_padho.py
@@ -0,0 +1,21 @@
+# Akhbaar padhke sunaao
+import requests
+import json
+
+def speak(str):
+    from win32com.client import Dispatch
+    speak = Dispatch("SAPI.SpVoice")
+    speak.Speak(str)
+
+if __name__ == '__main__':
+    speak("News for today.. Lets begin")
+    url = "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=4dbad97afb1f467b88320f20c20a6019"
+    news = requests.get(url).text
+    news_dict = json.loads(news)
+    arts = news_dict['articles']
+    for article in arts:
+        speak(article['title'])
+        print(article['title'])
+        speak("Moving on to the next news..Listen Carefully")
+
+    speak("Thanks for listening...")
